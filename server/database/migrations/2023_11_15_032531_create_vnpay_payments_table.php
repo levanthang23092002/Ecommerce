e62dd1c5cd7ab68d74f478882cd8bc120d0d6008
LD-Tien <leductien00@gmail.com>
2023-12-05 21:07:24 +0700

    Update schema

--- server/database/migrations/2023_11_15_032531_create_vnpay_payments_table.php
+++ server/database/migrations/2023_11_15_032531_create_vnpay_payments_table.php
@@ -27,7 +27,6 @@ return new class extends Migration
             $table->string('vnp_TxnRef');
             $table->string('vnp_SecureHash');
             $table->timestamps();
-            $table->foreign('vnp_TxnRef')->references('id')->on('orders')->onDelete('restrict');
         });
     }
 
